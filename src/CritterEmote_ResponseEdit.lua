b456fc3d43b0101913f28f62a4d179b03397c0e6
opussf <github@zz9-za.com>
2025-11-07 10:25:19 -0800

    Saves editbox to saveddata, and populates from saveddata.  90% happy.
    Still need to work on that edit box.

--- src/CritterEmote_ResponseEdit.lua
+++ src/CritterEmote_ResponseEdit.lua
@@ -3,11 +3,15 @@ function CritterEmote.Edit_OnLoad()
 	CritterEmoteResponseEditFrame_Title:SetText(CritterEmote.ADDONNAME)
 end
 function CritterEmote.Edit_OnShow()
-	CritterEmote.Log(CritterEmote.Error, "EditOnShow")
+	CritterEmote.Log(CritterEmote.Debug, "Edit_OnShow")
 	CritterEmote.Edit_UpdatePetInfo()
+	-- if drop down has emote - populate editbox
+	CritterEmote.Edit_PopulateEditBox()
 	CritterEmoteResponseEditFrame:RegisterEvent("COMPANION_UPDATE")
 end
 function CritterEmote.Edit_OnHide()
+	-- save editbox
+	CritterEmote.Edit_SaveEmotes()
 	CritterEmoteResponseEditFrame:UnregisterEvent("COMPANION_UPDATE")
 end
 function CritterEmote.Edit_COMPANION_UPDATE()
@@ -16,9 +20,12 @@ function CritterEmote.Edit_COMPANION_UPDATE()
 	-- CritterEmote.Edit_UpdatePetInfo()
 end
 function CritterEmote.Edit_UpdatePetInfo()
-	local petInfo = {C_PetJournal.GetPetInfoByPetID(C_PetJournal.GetSummonedPetGUID())}
-	CritterEmoteResponseEditFrame_Icon:SetTexture(petInfo[9])
-	CritterEmoteResponseEditFrame_Name:SetText((petInfo[2] or petInfo[8]))
+	local petGUID = C_PetJournal.GetSummonedPetGUID()
+	if petGUID then  -- @TODO: What to do about no pet?
+		local petInfo = {C_PetJournal.GetPetInfoByPetID(petGUID)}
+		CritterEmoteResponseEditFrame_Icon:SetTexture(petInfo[9])
+		CritterEmoteResponseEditFrame_Name:SetText((petInfo[2] or petInfo[8]))
+	end
 end
 function CritterEmote.Edit_InitEmoteDropDown(self)
 	if not CritterEmote.knownEmotes then
@@ -40,7 +47,7 @@ function CritterEmote.Edit_InitEmoteDropDown(self)
 		table.sort(CritterEmote.knownEmotes.keys)
 	end
 	UIDropDownMenu_Initialize(self, function(self, level, menuList) -- keep this an an anonymous function
-			-- this gets called MANY times
+			-- this gets called MANY times, this is the ONLY place that this is called.
 			local info = UIDropDownMenu_CreateInfo()
 
 			if (level or 1) == 1 then
@@ -58,25 +65,48 @@ function CritterEmote.Edit_InitEmoteDropDown(self)
 					UIDropDownMenu_AddButton(info, level)
 				end
 			end
-		end
-	)
+		end)
 	UIDropDownMenu_JustifyText(self, "LEFT")
 end
-function CritterEmote.Edit_SetEmoteForEdit(info)  -- takes the info table
+function CritterEmote.Edit_SetEmoteForEdit(info)  -- takes the info table, called when an emote is chosen
+	-- save editbox
 	CritterEmote.Edit_SaveEmotes()
 
+	-- update editEmote
 	CritterEmote.editEmote = info.value
-	print(CritterEmote.editEmote)
+	-- populate Editbox
+	CritterEmote.Edit_PopulateEditBox()
 	UIDropDownMenu_SetText(CritterEmoteResponseEditFrame_EmoteDropDown, CritterEmote.editEmote)
 	CloseDropDownMenus()
+	CritterEmoteResponseEditFrame_EditScrollFrame_EditBox:SetFocus()
 end
 function CritterEmote.Edit_SaveEmotes()
 	local numLines = CritterEmoteResponseEditFrame_EditScrollFrame_EditBox:GetNumLines()
-	print(numLines)
 	if numLines > 0 then
-		local text = CritterEmoteResponseEditFrame_EditScrollFrame_EditBox:GetText()
-	end
+		local petName = CritterEmoteResponseEditFrame_Name:GetText()
+		local editEmote = CritterEmote.editEmote
 
+		if petName then
+			-- clear old data, or make new
+			CritterEmote_CustomResponseEmotes[petName] = CritterEmote_CustomResponseEmotes[petName] or {}
+			CritterEmote_CustomResponseEmotes[petName][editEmote] = {} -- this is being replaced
+
+			-- populate data from editbox
+			local text = CritterEmoteResponseEditFrame_EditScrollFrame_EditBox:GetText().."\n"  -- append a newline
+			for emote in text:gmatch("(.-)\n") do
+				if emote ~= "" then
+					table.insert(CritterEmote_CustomResponseEmotes[petName][editEmote], emote)
+				end
+			end
+		end
+	end
+end
+function CritterEmote.Edit_PopulateEditBox()
+	local petName = CritterEmoteResponseEditFrame_Name:GetText()
+	local editEmote = CritterEmote.editEmote
+	CritterEmoteResponseEditFrame_EditScrollFrame_EditBox:SetText(
+			table.concat((CritterEmote_CustomResponseEmotes[petName] and CritterEmote_CustomResponseEmotes[petName][editEmote] or {}), "\n")
+		)
 end
 
 CritterEmote.commandList[CritterEmote.L["edit"]] = {
